3784ff2181039ef68b2d4d88b5f18e1c2e281439
KafuiAdaku <dennisadaku@ymail.com>
2023-11-05 00:06:35 -0700

    CORS implemented

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """A module that creates an instance of the flask framework"""
 from flask import Flask, jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 from os import getenv
 
 app = Flask(__name__)
+CORS(app, origins="0.0.0.0")
 app.register_blueprint(app_views)
 
 @app.teardown_appcontext
